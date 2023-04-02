50a8b422b9538cf913ead577c31cc7716af9ce3c
luisnarv <115510650+luisnarv@users.noreply.github.com>
2023-04-02 10:33:13 -0500

    Home

--- NetflixCop/src/App.jsx
+++ NetflixCop/src/App.jsx
@@ -12,6 +12,16 @@ function App() {
             <span>Netflix</span></div>
           <div className='login'><span>Iniciar seccion</span></div>
         </nav>
+        <div className='cabecerados'>
+          <h1 className='peliculas'>Películas y series ilimitadas y mucho más</h1>
+            <p className='disfruta'>Disfruta donde quieras. Cancela cuando quieras.</p>
+            <p className='disfruta'>¿Quieres ver Netflix ya? Ingresa tu email para crear una cuenta o reiniciar tu membresía de Netflix.</p>
+        <form className='for'>
+          <input type="text" name="email" id="" placeholder='Email' />
+          <button >Comenzar</button>
+        </form>
+        
+        </div>
       </section>
       <section>
 
