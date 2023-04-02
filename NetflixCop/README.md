d6181e4ce024587e6cc5b8bacdfd4bd4a387963d
luisnarv <115510650+luisnarv@users.noreply.github.com>
2023-04-02 09:18:27 -0500

    start

---
+++ NetflixCop/README.md
@@ -0,0 +1,15 @@
+# ABOUT THE PROJECT
+
+# Getting Started
+
+### Instalation
+
+Primero debemos instalar las dependencias de nuestra aplicacion
+
+1. Acceder al package.json 
+2. Abrir en la terminal integrada
+3. Ejecutar la siguiente linea de comando
+
+```
+npm install
+npm run dev 
---
+++ NetflixCop/src/App.jsx
@@ -0,0 +1,35 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div  >
+      <section className="cabecera">
+        <nav>
+          <div className='logo'>
+            <span>Netflix</span></div>
+          <div className='login'><span>Iniciar seccion</span></div>
+        </nav>
+      </section>
+      <section>
+
+      </section>
+      <section>
+
+      </section>
+      <section>
+
+      </section>
+      <section>
+
+      </section>
+      <section>
+
+      </section>
+    </div>
+  )
+}
+
+export default App
